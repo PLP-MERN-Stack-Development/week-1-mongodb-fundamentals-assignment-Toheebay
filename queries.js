3fa7db1d78a41c4655d99d13462d8b6177f98f4f
Toheebay <successphu@gmail.com>
2025-05-29 02:31:55 -0700

    Trigger workflow

--- queries.js
+++ queries.js
@@ -1,18 +1,16 @@
 const { MongoClient } = require('mongodb');
 
-// Replace with your actual MongoDB URI
-const uri = 'mongodb://localhost:27017'; // Use your MongoDB Atlas URI if you're not running locally
+const uri = 'mongodb://localhost:27017'; // GitHub Actions uses localhost
 const client = new MongoClient(uri);
 
 async function run() {
   try {
     await client.connect();
-    const db = client.db('plp_bookstore'); // Your database name
-
+    const db = client.db('plp_bookstore');
     const books = await db.collection('books').find({ genre: 'History' }).toArray();
     console.log(books);
   } catch (err) {
-    console.error('Error running query:', err);
+    console.error('Error:', err);
   } finally {
     await client.close();
   }
